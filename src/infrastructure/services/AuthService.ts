40c3e40aa49b912adcd899c5a927a4910ed79851
caetanojpo <106156212+caetanojpo@users.noreply.github.com>
2025-02-01 19:45:27 -0300

    feat: auth service

--- src/infrastructure/services/AuthService.ts
+++ src/infrastructure/services/AuthService.ts
@@ -4,8 +4,6 @@ import { env } from "../../config/env";
 import { IAuthService } from "../../domain/interfaces/IAuthService";
 import { IUser } from "../../domain/models/User";
 
-const JWT_SECRET = env.jwtSecret;
-
 export class AuthService implements IAuthService {
   async hashPassword(password: string): Promise<string> {
     return await bcrypt.hash(password, 10);
@@ -19,14 +17,14 @@ export class AuthService implements IAuthService {
   }
 
   generateToken(user: IUser): string {
-    return jwt.sign({ id: user.id, username: user.username }, JWT_SECRET, {
+    return jwt.sign({ id: user.id, email: user.email }, env.jwtSecret, {
       expiresIn: "1h",
     });
   }
 
   verifyToken(token: string): Promise<any> {
     return new Promise((resolve, reject) => {
-      jwt.verify(token, JWT_SECRET, (err, decoded) => {
+      jwt.verify(token, env.jwtSecret, (err, decoded) => {
         if (err) reject("Invalid Token");
         resolve(decoded);
       });
