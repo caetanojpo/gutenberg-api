58f5c25f4950470741c42b17e2fc036a10dbc4a5
caetanojpo <106156212+caetanojpo@users.noreply.github.com>
2025-02-01 19:44:25 -0300

    feat: change iUserRepository methods names

--- src/domain/interfaces/IUserRepository.ts
+++ src/domain/interfaces/IUserRepository.ts
@@ -3,11 +3,8 @@ import { IUser } from "../models/User";
 export interface IUserRepository {
   findById(id: string): Promise<IUser | null>;
   findByEmail(email: string): Promise<IUser | null>;
-  createUser(userData: {
-    username: string;
-    email: string;
-    password: string;
-  }): Promise<IUser>;
-  updateUser(id: string, userData: Partial<IUser>): Promise<IUser | null>;
-  deleteUser(id: string): Promise<void>;
+  save(user: IUser): Promise<void>;
+  update(id: string, userData: Partial<IUser>): Promise<void>;
+  softDelete(id: string): Promise<void>;
+  hardDelete(id: string): Promise<void>;
 }
