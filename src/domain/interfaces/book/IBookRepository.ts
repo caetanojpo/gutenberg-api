1468b90d5442a15da4f26f2f009c44d6e4d7cc15
caetanojpo <106156212+caetanojpo@users.noreply.github.com>
2025-02-02 18:25:32 -0300

    feat: book repository interface

---
+++ src/domain/interfaces/book/IBookRepository.ts
@@ -0,0 +1,10 @@
+import { IBook } from "../../models/Book";
+
+export interface IBookRepository {
+  findAll(): Promise<IBook[] | []>;
+  findAllByAuthor(author: string): Promise<IBook[] | []>;
+  findById(id: string): Promise<IBook | null>;
+  save(book: IBook): Promise<IBook | null>;
+  update(id: string, book: Partial<IBook>): Promise<void>;
+  delete(id: string): Promise<void>;
+}
