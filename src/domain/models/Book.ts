e36eae5771e2e10c08e21562c4ea77f317eb4779
caetanojpo <106156212+caetanojpo@users.noreply.github.com>
2025-02-02 18:21:46 -0300

    feat: book domain created

---
+++ src/domain/models/Book.ts
@@ -0,0 +1,32 @@
+export interface IBook {
+  id?: string;
+  title: string;
+  author: string;
+  coverPictureUrl: string;
+  content: string;
+  metadata: string;
+  createdAt?: Date;
+  updatedAt?: Date;
+}
+
+export class Book implements IBook {
+  id?: string;
+  title: string;
+  author: string;
+  coverPictureUrl: string;
+  content: string;
+  metadata: string;
+  createdAt?: Date;
+  updatedAt?: Date;
+
+  constructor(book: IBook) {
+    (this.id = book.id),
+      (this.title = book.title),
+      (this.author = book.author),
+      (this.coverPictureUrl = book.coverPictureUrl),
+      (this.content = book.content),
+      (this.metadata = book.metadata),
+      (this.createdAt = book.createdAt),
+      (this.updatedAt = book.updatedAt);
+  }
+}
