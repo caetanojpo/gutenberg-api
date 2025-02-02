627410b78782a25ce470aaf2e1bfecfbb870f289
caetanojpo <106156212+caetanojpo@users.noreply.github.com>
2025-02-02 18:14:37 -0300

    feat: controllers updated

--- src/adapters/controllers/AuthController.ts
+++ src/adapters/controllers/AuthController.ts
@@ -1,32 +1,66 @@
 import { Request, Response as ExpressResponse } from "express";
 import { LoginUser } from "../../application/use-cases/users/LoginUser";
 import { UserException } from "../../utils/exceptions/UserException";
-import logger from "../../config/logger";
-import { Response } from "../../utils/Response";
+import { logger } from "../../infrastructure/logger";
+import { Response } from "../../utils/helpers/Response";
+import { plainToInstance } from "class-transformer";
+import { LoginDTO } from "../../domain/dtos/Auth/LoginDTO";
+import { validate } from "class-validator";
+import { LoggerMessages } from "../../utils/helpers/LoggerMessages";
 
 export class AuthController {
   constructor(private loginCase: LoginUser) {}
 
   async login(req: Request, res: ExpressResponse): Promise<void> {
-    const { email, password } = req.body;
+    const loginData = plainToInstance(LoginDTO, req.body as object);
+    logger.logFormatted("info", LoggerMessages.LOGIN_ATTEMPT, loginData.email);
+    const errors = await validate(loginData);
 
-    if (!email || !password) {
-      logger.warn("Login attempt with missing email or password");
-      Response.error("Email and password are required", 400).send(res);
+    if (errors.length > 0) {
+      logger.logFormatted(
+        "error",
+        LoggerMessages.LOGIN_FAILED,
+        loginData.email
+      );
+      return Response.error(
+        "Email and password are required",
+        400,
+        errors
+      ).send(res);
     }
 
     try {
-      const token = await this.loginCase.execute(email, password);
-      logger.info(`User logged in successfully: ${email}`);
-      res.json({ token });
-      Response.success("User authenticated successfully", token, 201).send(res);
+      const token = await this.loginCase.execute(
+        loginData.email,
+        loginData.password
+      );
+      logger.logFormatted(
+        "info",
+        LoggerMessages.LOGIN_SUCCESS,
+        loginData.email
+      );
+      return Response.success(
+        "User authenticated successfully",
+        token,
+        201
+      ).send(res);
     } catch (error) {
       if (error instanceof UserException) {
-        logger.error(`User error during login: ${error.message}`);
-        Response.error("Failed to login: " + error.message, 400).send(res);
+        logger.logFormatted(
+          "error",
+          LoggerMessages.LOGIN_FAILED,
+          loginData.email
+        );
+        return Response.error("Failed to login: " + error.message, 400).send(
+          res
+        );
       } else {
-        logger.error("Unexpected error during login:", error);
-        Response.error("Internal server error", 500).send(res);
+        logger.logFormatted(
+          "error",
+          LoggerMessages.LOGIN_FAILED,
+          loginData.email
+        );
+        return Response.error("Internal server error", 500).send(res);
       }
     }
   }
--- src/adapters/controllers/UserController.ts
+++ src/adapters/controllers/UserController.ts
@@ -4,127 +4,187 @@ import { FindUser } from "../../application/use-cases/users/FindUser";
 import { UpdateUser } from "../../application/use-cases/users/UpdateUser";
 import { Request, Response as ExpressResponse } from "express";
 import { UserException } from "../../utils/exceptions/UserException";
-import { CreateUserDTO } from "../../domain/dtos/CreateUserDTO";
+import { CreateUserDTO } from "../../domain/dtos/User/CreateUserDTO";
 import { UserMapper } from "../../utils/mappers/UserMapper";
-import { UpdateUserDTO } from "../../domain/dtos/UpdateUserDTO";
-import { Response } from "../../utils/Response";
-import logger from "../../config/logger";
+import { UpdateUserDTO } from "../../domain/dtos/User/UpdateUserDTO";
+import { Response } from "../../utils/helpers/Response";
+import { logger } from "../../infrastructure/logger";
+import { plainToInstance } from "class-transformer";
+import { validate } from "class-validator";
+import { AuthService } from "../../infrastructure/services/AuthService";
+import { LoggerMessages } from "../../utils/helpers/LoggerMessages";
 
 export class UserController {
   constructor(
     private createCase: CreateUser,
     private findCase: FindUser,
     private deleteCase: DeleteUser,
-    private updateCase: UpdateUser
+    private updateCase: UpdateUser,
+    private auth: AuthService
   ) {}
 
   async createUser(req: Request, res: ExpressResponse): Promise<void> {
-    const userData: CreateUserDTO = req.body;
+    const userData = plainToInstance(CreateUserDTO, req.body as object);
+    logger.logFormatted(
+      "info",
+      LoggerMessages.START_USER_CREATION,
+      userData.email
+    );
+    const errors = await validate(userData);
+
+    if (errors.length > 0) {
+      logger.logFormatted(
+        "error",
+        LoggerMessages.VALIDATION_ERROR,
+        JSON.stringify(errors)
+      );
+      return Response.error("Validation failed", 400, errors).send(res);
+    }
+
     try {
-      logger.info("Creating user with email: " + userData.email);
       const user = await this.createCase.execute(userData);
-      Response.success("User created successfully", user, 201).send(res);
+
+      if (!user || !user.id) {
+        return Response.error("User not created", 400).send(res);
+      }
+
+      const token = this.auth.generateToken(user);
+
+      const authenticatedResponse = UserMapper.toLoginResponseDTO(
+        user.id,
+        token
+      );
+
+      return Response.success(
+        "User created successfully",
+        authenticatedResponse,
+        201
+      ).send(res);
     } catch (error) {
-      logger.error("Error in createUser: " + error);
       if (error instanceof UserException || error instanceof Error) {
-        Response.error("Failed to create user: " + error.message, 400).send(
-          res
-        );
+        return Response.error(
+          "Failed to create user: " + error.message,
+          400
+        ).send(res);
       } else {
-        Response.error("Internal server error", 500).send(res);
+        return Response.error("Internal server error", 500).send(res);
       }
     }
   }
 
   async getUserById(req: Request, res: ExpressResponse): Promise<void> {
     const userId = req.params.id;
+    logger.logFormatted("info", LoggerMessages.START_USER_FIND_BY_ID, userId);
     try {
       const user = await this.findCase.executeById(userId);
 
       if (!user) {
-        Response.error("User not found", 404).send(res);
-        return;
+        return Response.error("User not found", 404).send(res);
       }
 
       const getUserDTO = UserMapper.toGetUserDTOFromUser(user);
-      Response.success("User found", getUserDTO).send(res);
+      return Response.success("User found", getUserDTO, 200).send(res);
     } catch (error) {
       if (error instanceof UserException) {
-        Response.error("Failed to find user: " + error.message, 400).send(res);
+        return Response.error(
+          "Failed to find user: " + error.message,
+          400
+        ).send(res);
       } else {
-        Response.error("Internal server error", 500).send(res);
+        return Response.error("Internal server error", 500).send(res);
       }
     }
   }
 
   async getUserByEmail(req: Request, res: ExpressResponse): Promise<void> {
     const userEmail: string = req.params.email;
+    logger.logFormatted(
+      "info",
+      LoggerMessages.START_USER_FIND_BY_EMAIL,
+      userEmail
+    );
     try {
       const user = await this.findCase.executeByEmail(userEmail);
 
       if (!user) {
-        Response.error("User not found", 404).send(res);
-        return;
+        return Response.error("User not found", 404).send(res);
       }
 
       const getUserDTO = UserMapper.toGetUserDTOFromUser(user);
-      Response.success("User found", getUserDTO).send(res);
+      return Response.success("User found", getUserDTO).send(res);
     } catch (error) {
       if (error instanceof UserException) {
-        Response.error("Failed to find user: " + error.message, 400).send(res);
+        return Response.error(
+          "Failed to find user: " + error.message,
+          400
+        ).send(res);
       } else {
-        Response.error("Internal server error", 500).send(res);
+        return Response.error("Internal server error", 500).send(res);
       }
     }
   }
 
   async updateUser(req: Request, res: ExpressResponse): Promise<void> {
     const userId: string = req.params.id;
-    const userData: UpdateUserDTO = req.body;
+    const userData = plainToInstance(UpdateUserDTO, req.body as object);
+    logger.logFormatted("info", LoggerMessages.START_USER_UPDATE, userId);
+    const errors = await validate(userData);
+
+    if (errors.length > 0) {
+      return Response.error("Validation failed", 400, errors).send(res);
+    }
+
     try {
       await this.updateCase.execute(userId, userData);
-      Response.success("User updated successfully", null, 204).send(res);
+      return Response.success("User updated successfully", null, 204).send(res);
     } catch (error) {
       if (error instanceof UserException) {
-        Response.error("Failed to update user: " + error.message, 400).send(
-          res
-        );
+        return Response.error(
+          "Failed to update user: " + error.message,
+          400
+        ).send(res);
       } else {
-        Response.error("Internal server error", 500).send(res);
+        return Response.error("Internal server error", 500).send(res);
       }
     }
   }
 
   async softDeleteUser(req: Request, res: ExpressResponse): Promise<void> {
     const userId: string = req.params.id;
+    logger.logFormatted("info", LoggerMessages.START_USER_SOFT_DELETE, userId);
     try {
       await this.deleteCase.executeSoft(userId);
-      Response.success("User soft deleted successfully", null, 204).send(res);
+      return Response.success("User soft deleted successfully", null, 204).send(
+        res
+      );
     } catch (error) {
       if (error instanceof UserException) {
-        Response.error(
+        return Response.error(
           "Failed to soft delete user: " + error.message,
           400
         ).send(res);
       } else {
-        Response.error("Internal server error", 500).send(res);
+        return Response.error("Internal server error", 500).send(res);
       }
     }
   }
 
   async hardDeleteUser(req: Request, res: ExpressResponse): Promise<void> {
     const userId = req.params.id;
+    logger.logFormatted("info", LoggerMessages.START_USER_HARD_DELETE, userId);
     try {
       await this.deleteCase.executeHard(userId);
-      Response.success("User hard deleted successfully", null, 204).send(res);
+      return Response.success("User hard deleted successfully", null, 204).send(
+        res
+      );
     } catch (error) {
       if (error instanceof UserException) {
-        Response.error(
+        return Response.error(
           "Failed to hard delete user: " + error.message,
           400
         ).send(res);
       } else {
-        Response.error("Internal server error", 500).send(res);
+        return Response.error("Internal server error", 500).send(res);
       }
     }
   }
