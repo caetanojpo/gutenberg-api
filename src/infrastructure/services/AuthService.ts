c307cb813863c785220754c07a66d6a34f9f8426
caetanojpo <106156212+caetanojpo@users.noreply.github.com>
2025-02-02 18:12:04 -0300

    feat: auth service logger

--- src/infrastructure/services/AuthService.ts
+++ src/infrastructure/services/AuthService.ts
@@ -1,12 +1,14 @@
 import jwt from "jsonwebtoken";
 import bcrypt from "bcryptjs";
-import { env } from "../../config/env";
+import { env } from "../env";
 import { IAuthService } from "../../domain/interfaces/IAuthService";
 import { IUser } from "../../domain/models/User";
-import logger from "../../config/logger";
+import { logger } from "../logger";
+import { LoggerMessages } from "../../utils/helpers/LoggerMessages";
 
 export class AuthService implements IAuthService {
   async hashPassword(password: string): Promise<string> {
+    logger.logFormatted("info", LoggerMessages.HASHING_PASSWORD);
     return await bcrypt.hash(password, 10);
   }
 
@@ -14,20 +16,19 @@ export class AuthService implements IAuthService {
     password: string,
     hashedPassword: string
   ): Promise<boolean> {
+    logger.logFormatted("info", LoggerMessages.CHECKING_PASSWORD);
     return await bcrypt.compare(password, hashedPassword);
   }
 
   generateToken(user: IUser): string {
-    logger.info(
-      "Generating token with id: " + user.id,
-      " and email: " + user.email
-    );
+    logger.logFormatted("info", LoggerMessages.TOKEN_GENERATION, user.email);
     return jwt.sign({ id: user.id, email: user.email }, env.jwtSecret, {
       expiresIn: "1h",
     });
   }
 
   verifyToken(token: string): Promise<any> {
+    logger.logFormatted("info", LoggerMessages.TOKEN_VERIFICATION, token);
     return new Promise((resolve, reject) => {
       jwt.verify(token, env.jwtSecret, (err, decoded) => {
         if (err) reject("Invalid Token");
