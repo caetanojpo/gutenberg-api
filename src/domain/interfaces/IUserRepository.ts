0771a87cf23d068ae4b0e7e489ed8d6d035d3545
caetanojpo <106156212+caetanojpo@users.noreply.github.com>
2025-02-02 18:13:32 -0300

    feat: updated IUser interface

--- src/domain/interfaces/IUserRepository.ts
+++ src/domain/interfaces/IUserRepository.ts
@@ -3,7 +3,7 @@ import { IUser } from "../models/User";
 export interface IUserRepository {
   findById(id: string): Promise<IUser | null>;
   findByEmail(email: string): Promise<IUser | null>;
-  save(user: IUser): Promise<void>;
+  save(user: IUser): Promise<IUser | null>;
   update(id: string, userData: Partial<IUser>): Promise<void>;
   delete(id: string): Promise<void>;
 }
