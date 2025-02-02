ad50922667e576cdc1507d6347461b6443376291
caetanojpo <106156212+caetanojpo@users.noreply.github.com>
2025-02-02 18:11:04 -0300

    feat: mapper logger updated

--- src/utils/mappers/UserMapper.ts
+++ src/utils/mappers/UserMapper.ts
@@ -1,11 +1,18 @@
-import { plainToInstance } from "class-transformer";
-import { IUser, User } from "../../domain/models/User";
-import { CreateUserDTO } from "../../domain/dtos/CreateUserDTO";
-import { GetUserDTO } from "../../domain/dtos/GetUserDTO";
-import { UpdateUserDTO } from "../../domain/dtos/UpdateUserDTO";
+import { IUser } from "../../domain/models/User";
+import { CreateUserDTO } from "../../domain/dtos/User/CreateUserDTO";
+import { GetUserDTO } from "../../domain/dtos/User/GetUserDTO";
+import { UpdateUserDTO } from "../../domain/dtos/User/UpdateUserDTO";
+import { LoginResponseDTO } from "../../domain/dtos/Auth/LoginResponseDTO";
+import { logger } from "../../infrastructure/logger";
+import { LoggerMessages } from "../helpers/LoggerMessages";
 
 export class UserMapper {
   static toUserSchemaFromUser(user: IUser): any {
+    logger.logFormatted(
+      "info",
+      LoggerMessages.MAPPING_IUSER_TO_USER_SCHEMA,
+      user.username
+    );
     return {
       username: user.username,
       email: user.email,
@@ -14,7 +21,12 @@ export class UserMapper {
     };
   }
 
-  static toUserFromUserSchema(userSchema: any): User {
+  static toUserFromUserSchema(userSchema: any): IUser {
+    logger.logFormatted(
+      "info",
+      LoggerMessages.MAPPING_USER_SCHEMA_TO_IUSER,
+      userSchema.username
+    );
     return {
       id: userSchema._id.toString(),
       username: userSchema.username,
@@ -25,6 +37,11 @@ export class UserMapper {
   }
 
   static toUserFromCreateDto(createUserDTO: CreateUserDTO): IUser {
+    logger.logFormatted(
+      "info",
+      LoggerMessages.MAPPING_CREATE_USER_DTO_TO_IUSER,
+      createUserDTO.username
+    );
     return {
       username: createUserDTO.username,
       email: createUserDTO.email,
@@ -33,6 +50,11 @@ export class UserMapper {
   }
 
   static toUserFromUpdateDto(updateUserDTO: UpdateUserDTO): IUser {
+    logger.logFormatted(
+      "info",
+      LoggerMessages.MAPPING_UPDATE_USER_DTO_TO_IUSER,
+      updateUserDTO.username
+    );
     return {
       username: updateUserDTO.username,
       email: updateUserDTO.email,
@@ -41,11 +63,28 @@ export class UserMapper {
   }
 
   static toGetUserDTOFromUser(user: IUser): GetUserDTO {
+    logger.logFormatted(
+      "info",
+      LoggerMessages.MAPPING_IUSER_TO_GET_USER_DTO,
+      user.username
+    );
     return {
       id: user.id || "",
       username: user.username,
       email: user.email,
       isActive: user.isActive || false,
+    };
+  }
+
+  static toLoginResponseDTO(id: string, token: string): LoginResponseDTO {
+    logger.logFormatted(
+      "info",
+      LoggerMessages.MAPPING_TO_LOGIN_RESPONSE_DTO,
+      id
+    );
+    return {
+      id: id,
+      token: token,
     };
   }
 }
