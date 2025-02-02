b602e89e434df1fbd8502e32a473a5ff99b2dc62
caetanojpo <106156212+caetanojpo@users.noreply.github.com>
2025-02-02 18:10:52 -0300

    feat: index and server updated logger

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,15 @@
 import express from "express";
 import { config } from "dotenv";
-import { connectDB } from "./config/db/mongo";
-import { env } from "./config/env";
+import { connectDB } from "./infrastructure/database/mongo";
+import { env } from "./infrastructure/env";
+import { Request, Response as ExpressResponse, NextFunction } from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
-import logger from "./config/logger";
 import authRouter from "./adapters/routes/auth.routes";
 import userRouter from "./adapters/routes/user.routes";
+import { Response } from "./utils/helpers/Response";
+import { LoggerMessages } from "./utils/helpers/LoggerMessages";
+import { logger } from "./infrastructure/logger";
 
 config();
 
@@ -19,5 +22,13 @@ connectDB();
 // routes
 app.use("/users", userRouter);
 app.use("/auth", authRouter);
+
+app.use((req: Request, res: ExpressResponse, next: NextFunction): void => {
+  logger.logFormatted("error", LoggerMessages.ROUTE_NOT_FOUND, req.originalUrl);
+  Response.error(`Route ${req.method} ${req.originalUrl} not found`, 404).send(
+    res
+  );
+  next();
+});
 
 export default app;
